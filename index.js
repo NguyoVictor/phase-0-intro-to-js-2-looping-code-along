85cea9dc17ff32d5ce42c612a2a04099e19123cf
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2024-04-01 10:08:13 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const cards = ["Guadalupe","Ollie","Aki"];
+
+function writeCards(){
+   const thankYouMessages = [];
+    for(let i = 0; i < cards.length; i++){
+        thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+       }
+       return thankYouMessages;
+}
+
+writeCards();
+
+function countDown(count){
+    while (count >= 0 ){
+        console.log(count);
+        count--;
+    }
+}
+countDown();
